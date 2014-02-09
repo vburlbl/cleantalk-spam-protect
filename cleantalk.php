06650a5e56cdeb69cdfeb5ab8ddb9df42d5705fd
shagimuratov <shagimuratov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-09 04:13:27 +0000

    Fixed bug with CURLOPT_FOLLOWLOCATION.
    git-svn-id: https://plugins.svn.wordpress.org/cleantalk-spam-protect/trunk@854011 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cleantalk.php
+++ cleantalk.php
@@ -3,12 +3,12 @@
   Plugin Name: Anti-spam by CleanTalk 
   Plugin URI: http://cleantalk.org/my
   Description:  Cloud antispam for comments, registrations and contacts. The plugin doesn't use CAPTCHA, Q&A, math, counting animals or quiz to stop spam bots. 
-  Version: 2.32
+  Version: 2.33
   Author: СleanTalk <welcome@cleantalk.ru>
   Author URI: http://cleantalk.org
  */
 
-$ct_agent_version = 'wordpress-232';
+$ct_agent_version = 'wordpress-233';
 $ct_checkjs_frm = 'ct_checkjs_frm';
 $ct_checkjs_register_form = 'ct_checkjs_register_form';
 $ct_session_request_id_label = 'request_id';
@@ -1373,7 +1373,6 @@ function ct_admin_init() {
             
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
             $result = curl_exec($ch);
             curl_close($ch);
