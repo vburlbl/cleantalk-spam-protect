5468d07e7781cd138d05b0d3abfb9f6be7ff6b5a
shagimuratov <shagimuratov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-23 05:34:11 +0000

    2.51 ready to use.
    git-svn-id: https://plugins.svn.wordpress.org/cleantalk-spam-protect/trunk@936888 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cleantalk.php
+++ cleantalk.php
@@ -3,15 +3,14 @@
   Plugin Name: Anti-spam by CleanTalk
   Plugin URI: http://cleantalk.org
   Description:  Cloud antispam for comments, registrations and contacts. The plugin doesn't use CAPTCHA, Q&A, math, counting animals or quiz to stop spam bots. 
-  Version: 2.50
+  Version: 2.51
   Author: СleanTalk <welcome@cleantalk.ru>
   Author URI: http://cleantalk.org
  */
 
 define('CLEANTALK_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
-
-$ct_agent_version = 'wordpress-250';
+$ct_agent_version = 'wordpress-251';
 $ct_plugin_name = 'Anti-spam by CleanTalk';
 $ct_checkjs_frm = 'ct_checkjs_frm';
 $ct_checkjs_register_form = 'ct_checkjs_register_form';
